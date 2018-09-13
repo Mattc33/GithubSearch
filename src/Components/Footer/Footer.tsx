47a9838fdd811a9fb79a321538df59a0b2acb06d
Matt Chan <m.chan@obietechologies.com>
2018-09-13 17:24:47 -0400

    footer link

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import styles from './Footer.module.scss'
 const Footer = () => 
     <footer className={styles.footer}>
         <div className={styles.footerText}>
-            © Matthew Chan - Github Search Mini Project
+            © Matthew Chan - <a target="_blank" href="https://github.com/Mattc33/GithubSearch">Github Search Mini Project Repo</a>
         </div>
     </footer>
 
